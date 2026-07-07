8dc17dec5b455cf8cbb6a626771dd78cfa8ca257
Mark Ndwaru <marcosndwarunjoroge254@gmail.com>
2026-07-07 14:19:36 +0300

    completed on bufferedstreams

--- IOStreams/Buffered.java
+++ IOStreams/Buffered.java
@@ -1,16 +1,38 @@
+package IOStreams;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Buffered {
   public static void main(String[] args) {
-    try (BufferedReader br = new BufferedReader(new FileReader("filename.txt"))) {
+    try (BufferedWriter writing = new BufferedWriter(new FileWriter("filename.txt"))) {
+          writing.write("Hello");
+          writing.write("\n");
+          writing.write("lorem ipsum");
+          System.out.println("File written");
+      
+      }
+    
+    catch (IOException e) {
+      System.out.println("Error reading file.");
+    }
+    
+  
+    try(BufferedReader br = new BufferedReader(new FileReader("filename.txt"));) {
       String line;
       while ((line = br.readLine()) != null) {
         System.out.println(line);
-      }
-    } catch (IOException e) {
-      System.out.println("Error reading file.");
+        System.out.println("file read");
+    } }
+    catch(FileNotFoundException e){
+      System.err.println(e);
     }
+    catch(IOException e){
+      System.err.println(e);
+    }
+    
   }
 }
